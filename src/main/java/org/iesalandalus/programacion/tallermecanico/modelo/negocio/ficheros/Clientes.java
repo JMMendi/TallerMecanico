529a778304054d8c7c3ee6566ac778833af1df61
JMMendi <mendizabaly@gmail.com>
2024-04-17 19:54:40 +0200

    Implementados los métodos de los procesamientos de documento XML

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/ficheros/Clientes.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/ficheros/Clientes.java
@@ -2,17 +2,61 @@ package org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IClientes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.naming.OperationNotSupportedException;
+import javax.xml.parsers.DocumentBuilder;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Clientes implements IClientes {
+    private static final String FICHERO_CLIENTES = String.format("%s%s%s", "Datos", File.separator, "ficheroClientes.xml");
+    private static final String RAIZ = "DATOS";
+    private static final String CLIENTE = "cliente";
+    private static final String NOMBRE = "nombre";
+    private static final String DNI = "dni";
+    private static final String TELEFONO = "telefono";
+
     private final List<Cliente> coleccionClientes;
+    static Clientes instancia;
 
     public Clientes() {
         coleccionClientes = new ArrayList<>();
+    }
+    static Clientes getInstancia() {
+        if (instancia == null) {
+            instancia = new Clientes();
+        }
+        return instancia;
+    }
+    public void comenzar() {
+    }
+    private void procesarDocumentoXml(Document documentoXml){
+        Objects.requireNonNull(documentoXml, "El documento xml de clientes no puede ser nulo.");
+        UtilidadesXml.leerDocumentoXml(FICHERO_CLIENTES);
+    }
+    private Cliente getCliente(Element element) {
+        Document documentoXml = UtilidadesXml.leerDocumentoXml(FICHERO_CLIENTES);
+        String dni = null;
+        String nombre = null;
+        String telefono = null;
+        if (documentoXml.getDocumentElement().equals(element)) {
+            if (element.hasAttribute(DNI)) {
+                dni = element.getAttribute(DNI);
+            }
+            if (element.hasAttribute(NOMBRE)) {
+                nombre = element.getAttribute(NOMBRE);
+            }
+            if (element.hasAttribute(TELEFONO)) {
+                telefono = element.getAttribute(TELEFONO);
+            }
+        }
+        return new Cliente(nombre, dni, telefono);
     }
     public List<Cliente> get() {
         return coleccionClientes;
@@ -23,6 +67,27 @@ public class Clientes implements IClientes {
             throw new OperationNotSupportedException("Ya existe un cliente con ese DNI.");
         }
         coleccionClientes.add(cliente);
+    }
+    private Document crearDocumentoXml(){
+        DocumentBuilder constructor = UtilidadesXml.crearConstructorDocumentoXml();
+        Document documentoXml = null;
+        if (constructor != null) {
+            documentoXml = constructor.newDocument();
+        }
+        return documentoXml;
+    }
+    public void terminar() {
+
+    }
+    private Element getElemento(Document documentoXml, Cliente cliente) {
+        Objects.requireNonNull(documentoXml, "El documento xml de clientes no puede ser nulo.");
+        Objects.requireNonNull(cliente, "El cliente no puede ser nulo.");
+        Element elementoCliente = documentoXml.createElement(CLIENTE);
+        elementoCliente.setAttribute(NOMBRE, cliente.getNombre());
+        elementoCliente.setAttribute(DNI, cliente.getDni());
+        elementoCliente.setAttribute(TELEFONO, cliente.getTelefono());
+
+        return elementoCliente;
     }
     public boolean modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
         Objects.requireNonNull(cliente, "No se puede modificar un cliente nulo.");
--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/ficheros/Vehiculos.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/ficheros/Vehiculos.java
@@ -2,21 +2,88 @@ package org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IVehiculos;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import javax.naming.OperationNotSupportedException;
+import javax.xml.parsers.DocumentBuilder;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Vehiculos implements IVehiculos {
+    private static final String FICHERO_VEHICULOS = String.format("%s%s%s", "Datos", File.separator, "ficheroVehiculos.xml");
+    private static final String RAIZ = "Datos";
+    private static final String VEHICULO = "vehiculo";
+    private static final String MARCA = "marca";
+    private static final String MODELO = "modelo";
+    private static final String MATRICULA = "matricula";
+
+    static Vehiculos instancia;
     private List<Vehiculo> coleccionVehiculos;
 
     public Vehiculos() {
         coleccionVehiculos = new ArrayList<>();
     }
+    static Vehiculos getInstancia() {
+        if (instancia == null) {
+            instancia = new Vehiculos();
+        }
+        return instancia;
+    }
+    public void comenzar() {
+
+    }
+    private void procesarDocumentoXml(Document documentoXml) {
+        Objects.requireNonNull(documentoXml, "El documento xml de vehículos no puede ser nulo.");
+        UtilidadesXml.leerDocumentoXml(FICHERO_VEHICULOS);
+    }
+
     public List<Vehiculo> get() {
         return new ArrayList<>(coleccionVehiculos);
     }
+    private Vehiculo getVehiculo(Element elemento) {
+        Document documentoXml = UtilidadesXml.leerDocumentoXml(FICHERO_VEHICULOS);
+        String marca = null;
+        String modelo = null;
+        String matricula = null;
+
+        if (documentoXml.getDocumentElement().equals(elemento)) {
+            if (elemento.hasAttribute(MARCA)) {
+                marca = elemento.getAttribute(MARCA);
+            }
+            if (elemento.hasAttribute(MODELO)) {
+                modelo = elemento.getAttribute(MODELO);
+            }
+            if (elemento.hasAttribute(MATRICULA)) {
+                matricula = elemento.getAttribute(MATRICULA);
+            }
+        }
+        return new Vehiculo(marca, modelo, matricula);
+    }
+    public void terminar() {
+
+    }
+    private Document crearDocumentoXml() {
+        DocumentBuilder constructor = UtilidadesXml.crearConstructorDocumentoXml();
+        Document documentoXml = null;
+        if (constructor != null) {
+            documentoXml = constructor.newDocument();
+        }
+        return documentoXml;
+    }
+    private Element getElement(Document documentoXml, Vehiculo vehiculo) {
+        Objects.requireNonNull(documentoXml, "El documento xml de vehículos no puede ser nulo.");
+        Objects.requireNonNull(vehiculo, "El vehículo no puede ser nulo.");
+        Element elementoVehiculo = documentoXml.createElement(VEHICULO);
+        elementoVehiculo.setAttribute(MARCA, vehiculo.marca());
+        elementoVehiculo.setAttribute(MODELO, vehiculo.modelo());
+        elementoVehiculo.setAttribute(MATRICULA, vehiculo.matricula());
+
+        return elementoVehiculo;
+    }
+
     public void insertar(Vehiculo vehiculo) throws OperationNotSupportedException {
         Objects.requireNonNull(vehiculo, "No se puede insertar un vehículo nulo.");
         if (coleccionVehiculos.contains(vehiculo)) {
